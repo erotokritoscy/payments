482a7c2ed1e1514c5aaba05fdc6eb71ac538280e
erotokritoscy <erotokritos.cy@gmail.com>
2026-03-10 12:29:41 +0200

    store card & recurent payments

--- src/JCC.php
+++ src/JCC.php
@@ -4,7 +4,6 @@ namespace Erotokritoscy\Payments;
 
 use Erotokritoscy\Payments\Models\JccTransaction;
 use Exception;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
@@ -13,8 +12,15 @@ class JCC
     /**
      * @throws Exception
      */
-    public static function formUrl(float $amount, string $returnUrl, ?string $failUrl, ?string $callbackUrl, array $additionalParams = [], ?string $clientId = null): string
-    {
+    public static function formUrl(
+        float $amount,
+        string $returnUrl,
+        ?string $failUrl,
+        ?string $callbackUrl,
+        array $additionalParams = [],
+        ?string $clientId = null,
+        bool $forceCardStorage = false
+    ): string {
         $amount      = number_format($amount, 2, '', '');
         $orderNumber = time() . rand(1000, 9999);
 
@@ -24,7 +30,7 @@ class JCC
             $url = str_replace('-test', '', $url);
         }
 
-        $response = Http::asForm()->post($url, array_filter([
+        $params = [
             'userName'           => config('jcc.username'),
             'password'           => config('jcc.password'),
             'orderNumber'        => $orderNumber,
@@ -35,7 +41,13 @@ class JCC
             'dynamicCallbackUrl' => $callbackUrl,
             'clientId'           => $clientId,
             'jsonParams'         => $additionalParams !== [] ? json_encode($additionalParams) : null,
-        ]));
+        ];
+
+        if ($clientId && $forceCardStorage) {
+            $params['features'] = 'FORCE_CREATE_BINDING';
+        }
+
+        $response = Http::asForm()->post($url, array_filter($params));
 
         $data = $response->json();
         if (isset($data['errorCode'])) {
@@ -57,33 +69,49 @@ class JCC
     /**
      * @throws Exception
      */
-    public static function chargeWithBinding(float $amount, string $clientId, string $bindingId, ?string $tii = null): bool
-    {
+    public static function chargeWithBinding(
+        float $amount,
+        string $mdOrder,
+        string $clientId,
+        string $bindingId,
+        ?string $tii = null,
+    ) {
         $amount      = number_format($amount, 2, '', '');
         $orderNumber = time() . rand(1000, 9999);
 
         $url = config('jcc.order_binding_url');
+
+        $originalTransaction = JccTransaction::where('order_id', $mdOrder)->first();
+        if (!$originalTransaction) {
+            Log::error('JCC Error: Original transaction not found for mdOrder ' . $mdOrder);
+            throw new Exception('Original transaction not found');
+        }
+        $originalPaymentDate = strtotime($originalTransaction->created_at);
+        $originalPaymentNetRefNum = $originalTransaction->order_number;
 
         if (!config('jcc.development')) {
             $url = str_replace('-test', '', $url);
         }
 
         $response = Http::asForm()->post($url, [
-            'userName'  => config('jcc.username'),
-            'password'  => config('jcc.password'),
-            'mdOrder'   => $orderNumber,
-            'bindingId' => $bindingId,
-            'tii'       => $tii,
+            'userName'                 => config('jcc.username'),
+            'password'                 => config('jcc.password'),
+            'orderNumber'              => $orderNumber,
+            'mdOrder'                  => $mdOrder,
+            'bindingId'                => $bindingId,
+            'tii'                      => $tii,
+            'originalPaymentDate'      => $originalPaymentDate,
+            'originalPaymentNetRefNum' => $originalPaymentNetRefNum,
         ]);
 
         $data = $response->json();
-        if (isset($data['errorCode'])) {
+        if ($data['errorCode'] != 0) {
             Log::error('JCC Error: ' . $data['errorMessage']);
             throw new Exception('JCC Error');
         }
 
         $transaction                = new JccTransaction;
-        $transaction->order_id      = $data['orderId'];
+        $transaction->order_id      = $mdOrder;
         $transaction->order_number  = $orderNumber;
         $transaction->amount        = $amount;
         $transaction->currency_code = config('jcc.currencyCode');
@@ -92,7 +120,7 @@ class JCC
         $transaction->status        = $data['errorCode'] != 0 ? JccTransaction::STATUS_FAIL : JccTransaction::STATUS_SUCCESS;
         $transaction->save();
 
-        return $transaction->status === JccTransaction::STATUS_SUCCESS;
+        return $data;
     }
 
     public static function getOrderStatus($orderId)
